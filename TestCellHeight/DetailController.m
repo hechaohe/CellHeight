4033d0ea54d665ebf7e68a063a5f8e210d520eb6
hechaohe <550121308@qq.com>
2017-03-11 11:54:52 +0800

    添加悬浮按钮

--- TestCellHeight/DetailController.m
+++ TestCellHeight/DetailController.m
@@ -9,7 +9,8 @@
 #import "DetailController.h"
 
 @interface DetailController ()
-
+@property(strong,nonatomic)UIWindow *window;
+@property(strong,nonatomic)UIButton *button;
 @end
 
 @implementation DetailController
@@ -32,9 +33,33 @@
     [UIApplication sharedApplication].statusBarHidden = NO;
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;
     
-    
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Celll"];
     
+    
+    [self performSelector:@selector(createButton) withObject:nil afterDelay:1];
+
+    
+}
+- (void)createButton
+{
+    _button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_button setTitle:@"哈哈" forState:UIControlStateNormal];
+    _button.frame = CGRectMake(0, 0, 80, 80);
+    [_button addTarget:self action:@selector(resignWindow) forControlEvents:UIControlEventTouchUpInside];
+    _window = [[UIWindow alloc]initWithFrame:CGRectMake(100, [UIScreen mainScreen].bounds.size.height - 80, 80, 80)];
+    _window.windowLevel = UIWindowLevelAlert+1;
+    _window.backgroundColor = [UIColor redColor];
+    _window.layer.cornerRadius = 40;
+    _window.layer.masksToBounds = YES;
+    [_window addSubview:_button];
+    [_window makeKeyAndVisible];//关键语句,显示window
+    
+}
+- (void)resignWindow
+{
+    NSLog(@"haha");
+    [_window resignKeyWindow];
+    _window = nil;
     
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
